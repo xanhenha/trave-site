f03cdb77c78e0dd8c77a264659e3d79d7f3b29e7
Thiago Barbosa <tjbarbosa@uolinc.com>
2019-02-27 18:32:31 -0300

    Lesson 12 - gulp pipes e plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function(){
     console.log('DEFAULT');
@@ -10,9 +14,10 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-    console.log('style');
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([autoprefixer,cssvars,nested]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
-
 
 gulp.task('watch', function() {
     watch('./app/index.html', function(){
