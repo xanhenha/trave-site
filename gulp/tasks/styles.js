d20ab505e0d1a9c6e1be98e9e0c2b4bbeb4b8533
Xanhenha <xanhenha@gmail.com>
2019-02-28 11:39:42 -0300

    Lesson 18 - Tratando erros gulp sem quebra a task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -8,5 +8,9 @@ cssImport = require('postcss-import');
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
         .pipe(postcss([autoprefixer, cssImport, cssvars, nested]))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
         .pipe(gulp.dest('./app/temp/styles'));
 });
