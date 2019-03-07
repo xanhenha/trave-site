635bcde965946905e75e034d84be9e97a41dc269
Xanhenha <xanhenha@gmail.com>
2019-03-07 13:20:52 -0300

    Lesson 34 - GULP sprites - Part 2

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    svgSprite = require('gulp-svg-sprite');
+    svgSprite = require('gulp-svg-sprite'),
+    rename = require('gulp-rename');
 
 var config = {
     mode: {
         css: {
+            sprite : 'sprite.svg',
             render : {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -18,3 +20,16 @@ gulp.task('createSprite', function(){
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+})
+
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
