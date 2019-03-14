47bff4fddfdd453fa3d2919588f1c3b857d13164
Xanhenha <xanhenha@gmail.com>
2019-03-14 15:04:11 -0300

    Lesson 43/44/45 - Mobile header menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + ' now owes $0 in taxes.');
-    }
-}
-
-var john = new Person('John Doe', 'blue');
-john.greet();
-
-var jane = new Adult('Jane Smith', 'green');
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
